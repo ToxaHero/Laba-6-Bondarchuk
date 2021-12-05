fec28b3e70470ec58635215b861563e979188ea1
Anton <Anton@DESKTOP-HBP5BHV>
2021-12-05 18:23:11 +0200

    Добавьте файлы проекта.

---
+++ Лабораторна робота 6 XOR файл/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лабораторна_робота_6_XOR_файл_генератор_ключа
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
